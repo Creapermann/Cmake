75915b6e3ff419e3e272be2ac80077cc3e25d117
David <prtnprvtmail@protonmail.com>
2021-11-17 16:28:40 +0100

    Project 'Learning/03' removed

--- Learning/03/cmake/mymod.cmake
+++
@@ -1,6 +0,0 @@
-cmake_minimum_required(VERSION 3.8)
-
-
-function(mytest)
-    message("Yasss")    
-endfunction()
--- Learning/03/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cassert>
-
-int main()
-{
-    std::cout << "Hello world\n";
-    assert(1 == 2);
-    return 0;
-}
