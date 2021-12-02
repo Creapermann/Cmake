59b8a1c17684b46b2f8d2b691fc75be3aa1a1701
David <prtnprvtmail@protonmail.com>
2021-12-02 18:03:58 +0100

    Removed projects

--- Learning/07/src/selfMath/src/selfMath.cpp
+++
@@ -1,9 +0,0 @@
-#include "selfMath.hpp"
-
-
-int SelfMath::times_two(int n)
-{
-   return n*2; 
-}
-
-void hellos() { std::cout << "Hello\n"; }
--- Learning/08/src/app/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "MyLib.hpp"
-
-int main(int argc, char* argv[])
-{   
-    std::cout << "Hello World!\n";
-}
